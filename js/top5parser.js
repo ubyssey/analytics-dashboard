935f4fe9addea9d2580ea2196c4b36d94db3bdb9
Peter Siemens <peterjsiemens@gmail.com>
2017-03-12 18:30:32 -0700

    Clean up indentation, use single quotes

--- js/top5parser.js
+++ js/top5parser.js
@@ -1,43 +1,42 @@
-     function updateTopArticles() {
-                
-                console.log('refreshing top articles');
-                
-                var url = 'https://ubyssey-analytics.appspot.com/query?id=ahNzfnVieXNzZXktYW5hbHl0aWNzchULEghBcGlRdWVyeRiAgICA7a2SCgw&format=json';
-               
-                $.ajax({
-                    type: 'GET',
-                    url: url,
-                    dataType: 'jsonp',
-                    success: function(data) {
-                        renderHTML(data.rows);
-                    }
-                });
+function updateTopArticles() {
 
-                function renderHTML(data) {
-                    var date = new Date(Date.now());
-                    var hour = date.getHours();
-                    var title = "";
-                    var views = "";
-                    var i = 1;
-                    var counter = 0;
-                    while (i < 6) {
-                        title = data[counter][0];
-                        views = data[counter][3];
-                        article_hour = parseInt(data[counter][2]);
-                        if (article_hour == hour) {
-                            $("ol li:nth-child("+i+")").html(title);
-                            $("#article-" + i).html(views);
-                            i++;
-                        }
-                        counter++;
-                    }
+  console.log('refreshing top articles');
 
-                }
-            }
+  var url = 'https://ubyssey-analytics.appspot.com/query?id=ahNzfnVieXNzZXktYW5hbHl0aWNzchULEghBcGlRdWVyeRiAgICA7a2SCgw&format=json';
 
-            $("document").ready(function() {
-                updateTopArticles();
-                setInterval(updateTopArticles, 5000);
-            });
+  $.ajax({
+    type: 'GET',
+    url: url,
+    dataType: 'jsonp',
+    success: function(data) {
+      renderHTML(data.rows);
+    }
+  });
 
+  function renderHTML(data) {
+    var date = new Date(Date.now());
+    var hour = date.getHours();
+    var title = '';
+    var views = '';
+    var i = 1;
+    var counter = 0;
 
+    while (i < 6) {
+        title = data[counter][0];
+        views = data[counter][3];
+        article_hour = parseInt(data[counter][2]);
+        if (article_hour == hour) {
+          $('ol li:nth-child('+i+')').html(title);
+          $('#article-' + i).html(views);
+          i++;
+        }
+        counter++;
+    }
+
+  }
+}
+
+$('document').ready(function() {
+  updateTopArticles();
+  setInterval(updateTopArticles, 5000);
+});
