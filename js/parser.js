97e1f73108d72449c341f37eb075d288b2b79bfe
Peter Siemens <peterjsiemens@gmail.com>
2017-03-14 15:48:54 -0700

    Small fixes

--- js/parser.js
+++ js/parser.js
@@ -1,20 +1,20 @@
 function updateUsersOnSite() {
 
+  var url = 'https://ubyssey-analytics.appspot.com/query?id=ahNzfnVieXNzZXktYW5hbHl0aWNzchULEghBcGlRdWVyeRiAgICAgICACgw&format=json';
+  var $usersite = $('#usersite');
 
-var users = 'https://ubyssey-analytics.appspot.com/query?id=ahNzfnVieXNzZXktYW5hbHl0aWNzchULEghBcGlRdWVyeRiAgICAgICACgw&format=json';
-var $usersite = $('#usersite');
-
-$(function() {
-$.ajax({
-    type: 'GET',
-    url: 'https://ubyssey-analytics.appspot.com/query?id=ahNzfnVieXNzZXktYW5hbHl0aWNzchULEghBcGlRdWVyeRiAgICAgICACgw&format=json',
-	dataType: 'jsonp',
-    success: function(data) {
+  $(function() {
+    $.ajax({
+      type: 'GET',
+      url: url,
+	    dataType: 'jsonp',
+      success: function(data) {
         var d = new Date();
         var hour = d.getHours();
         $usersite.append(data.rows[hour][1]);
-    }
-});
-});
+      }
+    });
+  });
 }
+
 updateUsersOnSite();
