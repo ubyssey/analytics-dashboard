4a63d32d9ae024137e0a28ba3455f430b4ae8da8
Jason Smith <s.jason@shaw.ca>
2017-03-14 15:19:37 -0700

    wrapped in function

--- js/parser.js
+++ js/parser.js
@@ -1,24 +1,20 @@
+function updateUsersOnSite() {
 
-/*
-var usersOnSite = document.getElementById("user-site");
-*/
+
 var users = 'https://ubyssey-analytics.appspot.com/query?id=ahNzfnVieXNzZXktYW5hbHl0aWNzchULEghBcGlRdWVyeRiAgICAgICACgw&format=json';
 var $usersite = $('#usersite');
 
 $(function() {
 $.ajax({
     type: 'GET',
-    url: 'usersperhour.json',
-        xhrFields: {
-        withCredentials: true
-    },
+    url: 'https://ubyssey-analytics.appspot.com/query?id=ahNzfnVieXNzZXktYW5hbHl0aWNzchULEghBcGlRdWVyeRiAgICAgICACgw&format=json',
+	dataType: 'jsonp'
     success: function(data) {
         var d = new Date();
         var hour = d.getHours();
         var temp = JSON.parse(data);
         $usersite.append(temp.rows[hour][1]);
-        console.log(hour);
     }
 });
 });
-
+}
