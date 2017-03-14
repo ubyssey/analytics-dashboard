23bb2b27d042824979d12b315fb280252e1af9c8
JimFung <jfung92@gmail.com>
2017-03-14 15:14:04 -0700

    un es6 my code

--- js/pageviews.js
+++ js/pageviews.js
@@ -1,21 +1,21 @@
 // Endpoint for total number of views per hour
-const url = "https://ubyssey-analytics.appspot.com/query?id=ahNzfnVieXNzZXktYW5hbHl0aWNzchULEghBcGlRdWVyeRiAgICAr8iACgw&format=json"
+var viewsPerHour = "https://ubyssey-analytics.appspot.com/query?id=ahNzfnVieXNzZXktYW5hbHl0aWNzchULEghBcGlRdWVyeRiAgICAr8iACgw&format=json"
 
 // This runs once the page is ready to be loaded.
-$("document").ready(() => {
+$("document").ready(function() {
     pageviewTotalsPerHour();
     setInterval(pageviewTotalsPerHour, 5000);
 });
 
 // Make an HTTP call to the endpoint and update the pageviews element with the new number
-const pageviewTotalsPerHour = () => {
+function pageviewTotalsPerHour() {
   $.ajax({
     type: 'GET',
-    url: url,
+    url: viewsPerHour,
     dataType: 'jsonp',
-    success: data => {
-      const hour = new Date(Date.now()).getHours()
-      const pageviews = data.rows[hour][1]
+    success: function(data) {
+      var hour = new Date(Date.now()).getHours()
+      var pageviews = data.rGOows[hour][1]
       $('#page-views p').html(pageviews)
     }
   })
